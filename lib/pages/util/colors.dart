b7c8452211ca0ea42378bf949dba2bde01d29dc0
Ben Parisot <me@benparisot.com>
2020-03-29 10:57:15 -0700

    add colors util

---
+++ lib/pages/util/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color purple = Colors.deepPurple[200];
+Color cyan = Colors.cyan[100];
+Color indigo = Colors.indigo[400];
+Color pink = Colors.pink[100];
+Color yellow = Colors.amber[200];
+Color orange = Colors.deepOrange[200];
+Color green = Colors.tealAccent[100];
